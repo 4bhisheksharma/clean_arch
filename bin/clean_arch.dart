d5003a0ab7431688880185477e4883c01d6ebdff
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2026-03-10 16:07:27 +0545

    init: project initialization

---
+++ bin/clean_arch.dart
@@ -0,0 +1,5 @@
+import 'package:clean_arch/clean_arch.dart' as clean_arch;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clean_arch.calculate()}!');
+}
---
+++ test/clean_arch_test.dart
@@ -0,0 +1,8 @@
+import 'package:clean_arch/clean_arch.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
